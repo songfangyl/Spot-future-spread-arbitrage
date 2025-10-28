3d83ab8522c4e84ff9c5862fa2671842ae454c6b
Song Fang Yi <songfangyi1210@gmail.com>
2025-10-28 16:10:53 +0800

    Remove init file

--- quant_trader/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Helpers for the Quant Trader test tasks."""
-
-from .execution import SpreadExecutor, ExecutionConfig  # noqa: F401
-from .backtest import SpreadBacktester, BacktestConfig  # noqa: F401
